2fc91429405b846c699c1bc4d8b81641a0790d23
Hyun <eric0909kim@gmail.com>
2025-11-18 15:16:08 -0500

    skeleton rate limiter middleware

---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,18 @@
+
+const rateLimiter = (option) => {
+    return async (req, res, next) => {
+        try {
+            const allowed = checkRateLimit(req, option); // placeholder method
+
+            if (!allowed) {
+                return res.status(400).json({ message: "Too many requests" });
+            }
+
+            next();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+module.exports = rateLimiter;
