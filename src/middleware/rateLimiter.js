d1e6a8fb624eacdb29abe5e664a40f5d7757b263
Hyun <eric0909kim@gmail.com>
2025-11-19 09:50:22 -0500

    Updated rate limiter middleware to allow different methods of identification

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,9 +1,27 @@
 const checkRateLimit = require('../services/rateLimitService');
 
-const rateLimiter = (option) => {
+const rateLimiter = (configs) => {
     return async (req, res, next) => {
         try {
-            const allowed = await checkRateLimit(req, option);
+
+            let config = configs.find(c => {
+                return req.apiKey && c.identifier === "apiKey"
+                    || req.user?.id && c.identifier === "userId"
+            });
+            if (!config) {
+                config = configs.find(c => c.identifier === "ip");
+            }
+
+            let trackingKey;
+            if (config.identifier === "apiKey") {
+                trackingKey = req.apiKey;
+            } else if (config.identifier === "userId") {
+                trackingKey = req.user?.id;
+            } else {
+                trackingKey = req.ip;
+            }
+
+            const allowed = await checkRateLimit(trackingKey, config);
 
             if (!allowed) {
                 return res.status(400).json({ message: "Too many requests" });
