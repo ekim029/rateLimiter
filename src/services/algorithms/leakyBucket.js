9995e1507178ad002dbf1239d08663f6b474707d
Hyun <eric0909kim@gmail.com>
2025-11-18 20:41:40 -0500

    leaky bucket algorithm

---
+++ src/services/algorithms/leakyBucket.js
@@ -0,0 +1,33 @@
+
+let bucket = {}; // temp memory
+
+const leakyBucket = (req, option) => {
+    const { maxRequests, leakRate } = option;
+
+    const key = req.ip;
+    const now = Date.now();
+
+    if (!bucket[key]) {
+        bucket[key] = {
+            queue: 0,
+            lastCheck: now
+        };
+    } else {
+        const currBucket = bucket[key];
+
+        const timeElapsed = (now - currBucket.lastCheck) / 1000;
+        const tokenToRemove = timeElapsed * leakRate
+
+        currBucket.queue = Math.max(currBucket.queue - tokenToRemove, 0);
+        currBucket.lastCheck = now;
+    }
+
+    if (bucket[key].queue < maxRequests) {
+        bucket[key].queue += 1;
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = leakyBucket;
