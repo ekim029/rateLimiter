607f08b25b8a16440ddc0ef901e496017fe52dbe
Hyun <eric0909kim@gmail.com>
2025-11-20 08:32:25 -0500

    Updated fixed window algorithm to use lua scripts

--- src/algorithms/fixedWindow.js
+++ src/algorithms/fixedWindow.js
@@ -2,20 +2,22 @@ const redis = require('../redisClient');
 
 const fixedWindow = async (trackingKey, option) => {
     const { maxRequests, window } = option;
-
     let key = `fixedWindow:${trackingKey}`;
 
-    let count = await redis.incr(key);
+    const script = `
+        local max = tonumber(ARGV[1])
+        local window = tonumber(ARGV[2])
 
-    if (count === 1) {
-        await redis.pexpire(key, window)
-    }
+        local count = redis.call("INCR", KEYS[1])
+        if count == 1 then
+            redis.call("PEXPIRE", KEYS[1], window)
+        end
+        
+        return count <= max
+    `;
 
-    if (count > maxRequests) {
-        return false;
-    }
-
-    return true;
+    const allowed = await redis.eval(script, 1, key, maxRequests, window);
+    return allowed;
 }
 
 module.exports = fixedWindow;
